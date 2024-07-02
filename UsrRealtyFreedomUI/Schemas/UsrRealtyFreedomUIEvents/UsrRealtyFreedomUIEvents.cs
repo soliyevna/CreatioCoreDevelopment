551b70d406e33780a8625a39aeeed3a271beb98d
soliyevna <saparbaevazulaykho18@gmail.com>
2024-07-02 22:07:28 +0500

    hi

--- UsrRealtyFreedomUI/Schemas/UsrRealtyFreedomUIEvents/UsrRealtyFreedomUIEvents.cs
+++ UsrRealtyFreedomUI/Schemas/UsrRealtyFreedomUIEvents/UsrRealtyFreedomUIEvents.cs
@@ -1,2 +1,27 @@
-// my crazy code will be here!
-Console.WriteLine("Hello");
+namespace Terrasoft.Configuration
+{
+    using System;
+    using Terrasoft.Common;
+    using Terrasoft.Core.Entities;
+    using Terrasoft.Core.Entities.Events;
+    [EntityEventListener(SchemaName = "UsrRealtyFreedomUI")]
+    public class RealtyEntityEventListener : BaseEntityEventListener
+    {
+        public override void OnSaving(object sender, EntityBeforeEventArgs e)
+        {
+            base.OnSaving(sender, e);
+            Entity realty = (Entity)sender;
+            decimal price = realty.GetTypedColumnValue<decimal>("UsrPrice");
+            if (price > 1000000000)
+            {
+                e.IsCanceled = true;
+
+                string messageTemplate = new LocalizableString(realty.UserConnection.ResourceStorage, 
+                    "UsrRealtyFreedomUIEvents", "LocalizableStrings.ValueIsTooBig.Value").ToString();
+                
+                string message = string.Format(messageTemplate, "1.0B$");
+                throw new Exception(message);
+            }
+        }
+    }
+}
